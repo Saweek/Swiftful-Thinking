bd6b272b7643136a576658fe68f8f39b9ba252d5
SAWEEK <78707715+Saweek@users.noreply.github.com>
2024-03-16 16:02:17 +0100

    Finished Gradient Bootcamp File

--- GradientBootcamp.swift
+++ GradientBootcamp.swift
@@ -6,6 +6,35 @@ import SwiftUI
 
 struct Bootcamp: View {
     var body: some View {
+        RoundedRectangle(cornerRadius:25.0)
+            .fill(
+                // Color.red
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.red, Color.blue]), //colors
+                    startPoint: .topLeading, // where is the start
+                    endPoint: .bottom // where is the end
+                )
+            )
+            
+
+            .fill(
+                RadialGradient(
+                    gradient: Gradient(colors: [Color.red, Color.blue]), //colors
+                    center: .Leading, // wheere does in start
+                    startRadius: 5, // how big is the radius of the first color circle
+                    endRadius: 400 // how big is the radius of the last color circle
+                )
+            )
+
+
+            .fill(
+                AngularGradient(
+                    gradient: Gradient(colors: [Color.red, Color.blue]), //colors of the gradient
+                    center: .topLeading, // where is the beginning
+                    angle: .degrees(180 + 45) // where does the start pont look
+                )
+            )
+            .frame(width:300, height: 200)
     }
 }
 
