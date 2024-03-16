a2ce94d970fb11c6a0e2c91170cdf6b01c135249
SAWEEK <saweek.official@gmail.com>
2024-03-16 15:41:39 +0100

    Created file for Gradient

---
+++ GradientBootcamp.swift
@@ -0,0 +1,16 @@
+/*
+    Bootcamp
+*/
+
+import SwiftUI
+
+struct Bootcamp: View {
+    var body: some View {
+    }
+}
+
+struct Bootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextBootcamp()
+    }
+}
